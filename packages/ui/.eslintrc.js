1a455f2e84b5a252effaabec96cdbee94d6cf28b
Fahad Ahmad <fahadahmad63862@gmail.com>
2024-06-04 14:05:08 +0530

    rm eslint

--- packages/ui/.eslintrc.js
+++
@@ -1,8 +0,0 @@
-// /** @type {import("eslint").Linter.Config} */
-// module.exports = {
-//   root: true,
-//   extends: [require.resolve('@repo/eslint-config/node')],
-//   parserOptions: {
-//     project: `${__dirname}/tsconfig.json`,
-//   },
-// };
