184d18169a48152fd4655a7a08a960139eaac0a1
Fahad Ahmad <fahadahmad63862@gmail.com>
2024-06-18 20:51:06 +0530

    get & update ticker

--- backend/src/db/ticker.rs
+++ backend/src/db/ticker.rs
@@ -1,6 +1,8 @@
+use std::{ error::Error, str::FromStr };
+
 use scylla::transport::errors::QueryError;
 use rust_decimal_macros::dec;
-
+use rust_decimal::Decimal;
 use super::{ schema::{ Symbol, Ticker }, scylla_tables::ScyllaTicker, ScyllaDb };
 
 impl Ticker {
@@ -16,7 +18,7 @@ impl Ticker {
             quote_volume: dec!(0.0),
         }
     }
-    fn to_scylla_ticker(&self ) -> ScyllaTicker {
+    fn to_scylla_ticker(&self) -> ScyllaTicker {
         ScyllaTicker {
             symbol: self.symbol.to_string(),
             base_volume: self.base_volume.to_string(),
@@ -29,9 +31,21 @@ impl Ticker {
         }
     }
 }
-
+impl ScyllaTicker {
+    fn from_scylla_ticker(&self) -> Ticker {
+        Ticker {
+            symbol: self.symbol.to_string(),
+            base_volume: Decimal::from_str(&self.base_volume).unwrap(),
+            high_price: Decimal::from_str(&self.high_price).unwrap(),
+            last_price: Decimal::from_str(&self.last_price).unwrap(),
+            low_price: Decimal::from_str(&self.low_price).unwrap(),
+            price_change: Decimal::from_str(&self.price_change).unwrap(),
+            price_change_percent: Decimal::from_str(&self.price_change_percent).unwrap(),
+            quote_volume: Decimal::from_str(&self.quote_volume).unwrap(),
+        }
+    }
+}
 impl ScyllaDb {
-   
     pub async fn new_ticker(&self, ticker: Ticker) -> Result<(), QueryError> {
         let s =
             r#"
@@ -48,6 +62,57 @@ impl ScyllaDb {
         "#;
         let ticker = ticker.to_scylla_ticker();
         let res = self.session.query(s, ticker).await?;
+        Ok(())
+    }
+    pub async fn get_ticker(&self, symbol: Symbol) -> Result<Ticker, Box<dyn Error>> {
+        let s =
+            r#"
+            SELECT
+                symbol,
+                base_volume,
+                quote_volume,
+                price_change,
+                price_change_percent,
+                high_price,
+                low_price,
+                last_price
+            FROM keyspace_1.ticker_table
+            WHERE symbol = ? ;
+        "#;
+        let res = self.session.query(s, (symbol,)).await?;
+        let mut tickers = res.rows_typed::<ScyllaTicker>()?;
+        let scylla_ticker = tickers
+            .next()
+            .transpose()?
+            .ok_or(QueryError::InvalidMessage("Ticker does not exist in db".to_string()))?;
+        let ticker = scylla_ticker.from_scylla_ticker();
+        Ok(ticker)
+    }
+    pub async fn update_ticker(&self, ticker: &mut Ticker) -> Result<(), Box<dyn Error>> {
+        let ticker = ticker.to_scylla_ticker();
+        let s =
+            r#"
+            UPDATE keyspace_1.ticker_table 
+            SET
+                base_volume = ?,
+                quote_volume = ?,
+                price_change = ?,
+                price_change_percent = ?,
+                high_price = ?,
+                low_price = ?,
+                last_price = ?
+            WHERE symbol = ? ;
+        "#;
+        let res = self.session.query(s, (
+            ticker.base_volume,
+            ticker.quote_volume,
+            ticker.price_change,
+            ticker.price_change_percent,
+            ticker.high_price,
+            ticker.low_price,
+            ticker.last_price,
+            ticker.symbol,
+        )).await?;
         Ok(())
     }
 }
