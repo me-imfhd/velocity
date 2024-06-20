a98e486a24a9f6e2c5625107f69de37cb43a8dd3
Fahad Ahmad <fahadahmad63862@gmail.com>
2024-06-20 21:21:20 +0530

    orders getting processed and trades getting queued realtime sucess

--- matching-engine/src/app.rs
+++ matching-engine/src/app.rs
@@ -8,17 +8,7 @@ use serde::{ Deserialize, Serialize };
 use crate::{
     config::GlobalConfig,
     matching_engine::{ self, engine::MatchingEngine, orderbook::OrderSide, Id },
-    routes::{
-        engine::{
-            add_new_market,
-            fill_limit_order,
-            fill_market_order,
-            get_asks,
-            get_bids,
-            get_quote,
-        },
-        health::health_check,
-    },
+    routes::{ engine::{ add_new_market, get_asks, get_bids, get_quote }, health::health_check },
 };
 
 pub struct Application {
@@ -53,7 +43,6 @@ async fn run(listener: TcpListener) -> Result<actix_web::dev::Server, std::io::E
     let mut matching_engine = MatchingEngine::init();
     matching_engine.recover_all_orderbooks(&mut redis_connection);
 
-    // dbg!(&matching_engine, &users); recovered app state
     let app_state = web::Data::new(AppState {
         matching_engine: Mutex::new(matching_engine),
         redis_connection: Mutex::new(redis_connection),
@@ -61,6 +50,7 @@ async fn run(listener: TcpListener) -> Result<actix_web::dev::Server, std::io::E
     let worker_app_state = app_state.clone();
     let matching_engine = worker_app_state.matching_engine.lock().unwrap();
     let symbols = matching_engine.registered_exchanges();
+    drop(matching_engine); // if not, then it will deadlock the matching_engine mutex
     symbols.par_iter().for_each(|symbol| {
         let sym4asks = symbol.clone();
         let asks_worker_app_state = worker_app_state.clone();
@@ -70,18 +60,15 @@ async fn run(listener: TcpListener) -> Result<actix_web::dev::Server, std::io::E
         rayon::spawn(process_order(sym4asks, asks_worker_app_state, OrderSide::Ask));
     });
     let server = HttpServer::new(move || {
-        App::new()
-            .service(health_check)
-            .service(
-                scope("/api/v1")
-                    .app_data(app_state.clone())
-                    .service(add_new_market)
-                    .service(fill_limit_order)
-                    .service(fill_market_order)
-                    .service(get_asks)
-                    .service(get_bids)
-                    .service(get_quote)
-            )
+        App::new().service(
+            scope("/api/v1")
+                .app_data(app_state.clone())
+                .service(health_check)
+                .service(add_new_market)
+                .service(get_asks)
+                .service(get_bids)
+                .service(get_quote)
+        )
     })
         .listen(listener)?
         .run();
@@ -89,7 +76,7 @@ async fn run(listener: TcpListener) -> Result<actix_web::dev::Server, std::io::E
     Ok(server)
 }
 
-fn connect_redis(url: &str) -> Connection {
+pub fn connect_redis(url: &str) -> Connection {
     let client = redis::Client::open(url).expect("Could not create client.");
     let mut connection = client.get_connection().expect("Could not connect to the client");
     return connection;
@@ -100,6 +87,11 @@ fn process_order(symbol: String, app_state: Data<AppState>, order_side: OrderSid
         println!("{}s Worker Thread Created For {}", order_side.to_string(), symbol);
         loop {
             let con = &mut app_state.redis_connection.lock().unwrap();
+            // right now two orders of different exchanges might be running sequentially instead of parallely-
+            // since the entire matching engine gets locked for processing single order.
+            // (maybe my guess), this can be made more performant by using mutex on each orderbook-
+            // instead of locking complete matching engine,
+            let mut matching_engine = app_state.matching_engine.lock().unwrap();
             let result = redis
                 ::cmd("RPOP")
                 .arg(format!("queues:{}:{}", order_side.to_string(), symbol))
@@ -111,6 +103,7 @@ fn process_order(symbol: String, app_state: Data<AppState>, order_side: OrderSid
                         order_side.to_string(),
                         symbol
                     );
+                    matching_engine.process_order(&order_string, con);
                 }
                 Err(_) => {
                     println!("{}s Task queue empty, symbol: {}", order_side.to_string(), symbol);
