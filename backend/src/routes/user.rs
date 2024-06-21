f17ef9ffa19c98f33c5de6ba44d045dd9d61d63a
Fahad Ahmad <fahadahmad63862@gmail.com>
2024-06-21 09:27:58 +0530

    get_user and deposit balances api

--- backend/src/routes/user.rs
+++ backend/src/routes/user.rs
@@ -1,6 +1,7 @@
-use actix_web::{ web::Data, HttpResponse };
+use actix_web::{ web::{ Data, Json, Query }, HttpResponse };
+use serde::{ Deserialize, Serialize };
 
-use crate::{ app::AppState, db::schema::User };
+use crate::{ api::user, app::AppState, db::schema::{ Asset, Id, Quantity, User } };
 
 #[actix_web::post("/new_user")]
 pub async fn new_user(app_state: Data<AppState>) -> HttpResponse {
@@ -16,6 +17,40 @@ pub async fn new_user(app_state: Data<AppState>) -> HttpResponse {
                 Err(err) => HttpResponse::InternalServerError().json(err.to_string()),
             }
         }
-        Err(err) => HttpResponse::InternalServerError().json(err.to_string())
+        Err(err) => HttpResponse::InternalServerError().json(err.to_string()),
+    }
+}
+
+#[derive(Serialize, Deserialize)]
+struct QueryId {
+    id: Id,
+}
+#[actix_web::get("/get_user")]
+pub async fn get_user(query: Query<QueryId>, app_state: Data<AppState>) -> actix_web::HttpResponse {
+    let s_db = app_state.scylla_db.lock().unwrap();
+    let result = s_db.get_user(query.id).await;
+    match result {
+        Ok(user) => HttpResponse::Ok().json(user),
+        Err(err) => HttpResponse::NotFound().json(format!("User Not Found\n {}", err.to_string())),
+    }
+}
+
+#[derive(Serialize, Deserialize)]
+struct Deposit {
+    user_id: Id,
+    asset: Asset,
+    quantity: Quantity,
+}
+#[actix_web::post("/deposit")]
+pub async fn deposit(body: Json<Deposit>, app_state: Data<AppState>) -> actix_web::HttpResponse {
+    let s_db = app_state.scylla_db.lock().unwrap();
+    let mut result = s_db.get_user(body.user_id).await;
+    match result {
+        Ok(mut user) => {
+            user.deposit(&body.asset, body.quantity);
+            s_db.update_user(&mut user).await.unwrap();
+            HttpResponse::Ok().json(user)
+        }
+        Err(err) => HttpResponse::NotFound().json(format!("User Not Found\n {}", err.to_string())),
     }
 }
