043a22079655073f32c4033ce5dd63ae209976db
Fahad Ahmad <fahadahmad63862@gmail.com>
2024-07-04 00:00:20 +0530

    interacts with engine for user entries & balances

--- backend/src/routes/user.rs
+++ backend/src/routes/user.rs
@@ -6,9 +6,11 @@ use crate::{ api::user, app::AppState, db::schema::{ Asset, Id, Quantity, User }
 #[actix_web::post("/new")]
 pub async fn new_user(app_state: Data<AppState>) -> HttpResponse {
     let s_db = app_state.scylla_db.lock().unwrap();
-    let result = s_db.new_user_id().await;
-    match result {
-        Ok(id) => {
+    let reqwest = app_state.reqwest.lock().unwrap();
+    let response = reqwest.post("http://127.0.0.1:5000/api/v1/user/new").send().await;
+    match response {
+        Ok(response) => {
+            let id: i64 = response.json().await.unwrap();
             let u = User::new(id);
             let user_created = u.clone();
             let user = s_db.new_user(u).await;
@@ -44,15 +46,23 @@ struct Deposit {
 #[actix_web::post("/deposit")]
 pub async fn deposit(body: Json<Deposit>, app_state: Data<AppState>) -> actix_web::HttpResponse {
     let s_db = app_state.scylla_db.lock().unwrap();
-    let mut result = s_db.get_user(body.user_id).await;
-    match result {
-        Ok(mut user) => {
-            user.deposit(&body.asset, body.quantity);
-            s_db.update_user(&mut user).await.unwrap();
-            HttpResponse::Ok().json(user)
-        }
-        Err(err) => HttpResponse::NotFound().json(format!("User Not Found\n {}", err.to_string())),
+    let reqwest = app_state.reqwest.lock().unwrap();
+    if let Err(err) = s_db.get_user(body.user_id).await {
+        return HttpResponse::NotFound().json(format!("User Not Found\n {}", err));
     }
+
+    if
+        let Ok(response) = reqwest
+            .post("http://127.0.0.1:5000/api/v1/user/deposit")
+            .json(&body.0)
+            .send().await
+    {
+        let mut user: User = response.json().await.unwrap();
+        s_db.update_user(&mut user).await.unwrap();
+        return HttpResponse::Ok().json(user);
+    }
+
+    HttpResponse::InternalServerError().json("Try again later, matching engine is down")
 }
 #[derive(Serialize, Deserialize)]
 struct Withdraw {
@@ -63,15 +73,27 @@ struct Withdraw {
 #[actix_web::post("/withdraw")]
 pub async fn withdraw(body: Json<Withdraw>, app_state: Data<AppState>) -> actix_web::HttpResponse {
     let s_db = app_state.scylla_db.lock().unwrap();
+    let reqwest = app_state.reqwest.lock().unwrap();
     let mut result = s_db.get_user(body.user_id).await;
     match result {
         Ok(mut user) => {
             if body.quantity > user.available_balance(&body.asset).unwrap() {
                 return HttpResponse::NotAcceptable().json("Over Withdrawing");
             }
-            user.withdraw(&body.asset, body.quantity);
-            s_db.update_user(&mut user).await.unwrap();
-            HttpResponse::Ok().json(user)
+            if
+                let Ok(response) = reqwest
+                    .post("http://127.0.0.1:5000/api/v1/user/withdraw")
+                    .json(&body.0)
+                    .send().await
+            {
+                let mut user: User = response.json().await.unwrap();
+                s_db.update_user(&mut user).await.unwrap();
+                return HttpResponse::Ok().json(user);
+            } else {
+                return HttpResponse::InternalServerError().json(
+                    "Try again later, matching engine is down"
+                );
+            }
         }
         Err(err) => HttpResponse::NotFound().json(format!("User Not Found\n {}", err.to_string())),
     }
